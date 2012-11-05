1a73ad8720aca35cb64e0fe41c68990be344272c
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-11-05 21:03:23 +0100

    switched back to install, will run with -U anyway, also changed file existance test to test -f instead of File.exists

--- providers/rpmbuild.rb
+++ providers/rpmbuild.rb
@@ -6,7 +6,7 @@ action :install do
   remote_file "#{node["qmailtoaster"]["workspace"]}/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm" do
     source "http://mirrors.qmailtoaster.com/current/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm"
     mode 640
-    not_if { ::File.exists?("#{node["qmailtoaster"]["workspace"]}/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm") }
+    not_if "test -f #{node["qmailtoaster"]["workspace"]}/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm"
   end
   execute "build_#{new_resource.name}" do
     command "rpmbuild --rebuild --with cnt50#{node["qmailtoaster"]["rpmbuild"]["distro"]} #{node["qmailtoaster"]["workspace"]}/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm"
@@ -15,7 +15,7 @@ action :install do
 
   # Install it
   package new_resource.name do
-    action :upgrade
+    action :install
     source "#{node["qmailtoaster"]["workspace"]}/RPMS/#{new_resource.arch}/#{new_resource.name}-#{new_resource.version}.#{new_resource.arch}.rpm"
     provider Chef::Provider::Package::Rpm
     not_if "rpm -qi #{new_resource.name}"
