61d0645a9f0cc19fa2c8ed6e2f3d8c5668f5aea6
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-11-06 16:32:35 +0100

    make rpmbuild pass foodcritic checks

--- providers/rpmbuild.rb
+++ providers/rpmbuild.rb
@@ -5,19 +5,20 @@ action :install do
   Chef::Log.info("Compiling #{new_resource.name} at version #{new_resource.version} #{node["qmailtoaster"]["rpmbuild"]["distro"]}")
   remote_file "#{node["qmailtoaster"]["workspace"]}/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm" do
     source "http://mirrors.qmailtoaster.com/current/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm"
-    mode 640
+    mode "640"
     not_if "test -f #{node["qmailtoaster"]["workspace"]}/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm"
   end
-  execute "build_#{new_resource.name}" do
+  r = execute "build_#{new_resource.name}" do
     command "rpmbuild --rebuild --with #{node["qmailtoaster"]["rpmbuild"]["with"]} #{node["qmailtoaster"]["workspace"]}/SRPMS/#{new_resource.name}-#{new_resource.version}.src.rpm"
     not_if "rpm -qi #{new_resource.name}"
   end
-
+  new_resource.updated_by_last_action(true) if r.updated_by_last_action?
   # Install it
-  package new_resource.name do
+  r = package new_resource.name do
     action :install
     source "#{node["qmailtoaster"]["workspace"]}/RPMS/#{new_resource.arch}/#{new_resource.name}-#{new_resource.version}.#{new_resource.arch}.rpm"
     provider Chef::Provider::Package::Rpm
     not_if "rpm -qi #{new_resource.name}"
   end
+  new_resource.updated_by_last_action(true) if r.updated_by_last_action?
 end
