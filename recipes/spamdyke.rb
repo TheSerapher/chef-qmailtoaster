11d0aae9ed79209d2c27503aacc1f48a32d6cfc7
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-11-08 17:01:32 +0100

    include standard qmailtoaster recipe to ensure it is installed

--- recipes/spamdyke.rb
+++ recipes/spamdyke.rb
@@ -7,12 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-if node.recipe?('qmailtoaster')
-  include_recipe "qmailtoaster::_pre_setup_spamdyke"
-  include_recipe "qmailtoaster::_install_spamdyke"
-  include_recipe "qmailtoaster::_configure_spamdyke"
-  include_recipe "qmailtoaster::_test_spamdyke"
-else
-  log("Ensure your node includes the qmailtoaster::default recipe.") { level :error }
-end
+include_recipe "qmailtoaster"
+include_recipe "qmailtoaster::_pre_setup_spamdyke"
+include_recipe "qmailtoaster::_install_spamdyke"
+include_recipe "qmailtoaster::_configure_spamdyke"
+include_recipe "qmailtoaster::_test_spamdyke"
 
