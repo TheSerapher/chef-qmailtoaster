a4184b38e7f9558cd187cfd5a94639498d15e249
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-11-09 16:21:03 +0100

    changing RPM removal to nodeps to allow crontab etc. to remain installed

--- spec/_pre_setup_spec.rb
+++ spec/_pre_setup_spec.rb
@@ -4,12 +4,9 @@ describe 'qmailtoaster::_pre_setup' do
   before (:all) {
     @chef_run = ChefSpec::ChefRunner.new.converge 'qmailtoaster::_pre_setup'
   }
-  %w{ postfix exim }.each do |pkg|
+  %w{ postfix exim sendmail }.each do |pkg|
     it "should remove package #{pkg}" do
       @chef_run.should remove_package pkg
     end
-  end
-  it "should install package ssmtp" do
-    @chef_run.should install_package "ssmtp"
   end
 end
