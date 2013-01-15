bdada23e9032bd84cc2715652892e570d712e5e4
Sebastian Grewe <sebgrewe@bigpoint.net>
2013-01-15 15:02:22 +0100

    merged from script testing to proper Rakefile testing

--- spec/spamdyke_spec.rb
+++ spec/spamdyke_spec.rb
@@ -3,6 +3,7 @@ require 'chefspec'
 describe 'qmailtoaster::spamdyke' do
   before (:all) {
     @chef_run = ChefSpec::ChefRunner.new
+    @chef_run.node.set["mysql"] = { "server_root_password" => "root", "server_debian_password" => "debian", "server_repl_password" => "repl" }
     @chef_run.converge 'qmailtoaster::spamdyke'
   }
   %w{
