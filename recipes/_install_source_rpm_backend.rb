f8e096989b4305592c0b59b163e649a36510d6c4
Sebastian Grewe <sebgrewe@bigpoint.net>
2013-02-10 20:01:31 +0100

    fixing installation conflict during rpmbuild process

--- recipes/_install_source_rpm_backend.rb
+++ recipes/_install_source_rpm_backend.rb
@@ -9,6 +9,14 @@
 # Include required recipes
 
 node['qmailtoaster']['packages']['backend']['sources'].each do |data|
+  # When mrtg is installed exim gets re-installed
+  # Remove this again as soon as we are able to
+  if data['name'] == 'qmail-toaster'
+    rpm_package 'exim' do
+      options '--no-deps'
+      action :remove
+    end
+  end
   qmailtoaster_rpmbuild data['name'] do
     version data['version']
     arch data['arch']
