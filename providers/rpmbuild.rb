e6d5783a839ef1d7763c5c030c110d0c2c95a147
Sebastian Grewe <sebastian@grewe.ca>
2013-10-17 17:02:12 +0200

    [IMPEOVED] Package installation process
    * Added package checksum to remote_file
    * Added notification chain for downloaded files
    
    This will allow installations to properly continue on partial downloads.

--- providers/rpmbuild.rb
+++ providers/rpmbuild.rb
@@ -9,27 +9,24 @@ action :install do
   else
     rpmbuild_opts = '--with cnt50'
   end
-  Chef::Log.info("Compiling #{new_resource.name} with #{rpmbuild_opts} " +
-                 "at version #{new_resource.version}")
   remote_file "#{node['qmailtoaster']['workspace']}/SRPMS/" +
               "#{new_resource.name}-#{new_resource.version}.src.rpm" do
     source 'http://mirrors.qmailtoaster.com/current/SRPMS/' +
            "#{new_resource.name}-#{new_resource.version}.src.rpm"
+    checksum new_resource.checksum if new_resource.checksum
     mode '640'
-    not_if do
-      "test -f #{node['qmailtoaster']['workspace']}/SRPMS/" +
-        "#{new_resource.name}-#{new_resource.version}.src.rpm"
-    end
+    notifies :run, "execute[build_#{new_resource.name}]", :immediately
   end
   r = execute "build_#{new_resource.name}" do
+    action :nothing
     command "rpmbuild --rebuild #{rpmbuild_opts} #{node['qmailtoaster']['workspace']}/SRPMS/" +
             "#{new_resource.name}-#{new_resource.version}.src.rpm"
-    not_if "rpm -qi #{new_resource.name}"
+    notifies :install, "package[#{new_resource.name}]", :immediately
   end
   new_resource.updated_by_last_action(true) if r.updated_by_last_action?
   # Install it
   r = package new_resource.name do
-    action :install
+    action :nothing
     source "#{node['qmailtoaster']['workspace']}/RPMS/#{new_resource.arch}/" +
            "#{new_resource.name}-#{new_resource.version}.#{new_resource.arch}.rpm"
     provider Chef::Provider::Package::Rpm
--- resources/rpmbuild.rb
+++ resources/rpmbuild.rb
@@ -3,4 +3,5 @@ default_action :install
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :version, :kind_of => String, :required => true
+attribute :checksum, :kind_of => String, :required => true
 attribute :arch, :kind_of => String, :default => node['kernel']['machine']
